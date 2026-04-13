e67b3f5ceb5f391e8c4cd17f629cb4417f0d9d76
Marta Kule <mkule-mac@mkule-mac.local>
2026-04-13 14:06:21 -0400

    exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ word }) {
-  const cells = word ? word.split("") : range(5).map(() => "");
+function Guess({ word, answer }) {
+  const cells = word ? checkGuess(word, answer) : range(5).map(() => "");
   return (
     <p className="guess">
       {cells.map((letter, index) => (
-        <span key={index} className="cell">
-          {letter || ""}
+        <span key={index} className={`cell ${letter.status || ""}`}>
+          {letter.letter || ""}
         </span>
       ))}
     </p>
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -5,11 +5,11 @@ import Guess from "../Guess/Guess";
 
 const rows = range(NUM_OF_GUESSES_ALLOWED);
 
-function GuessList({ guesses }) {
+function GuessList({ guesses, answer }) {
   return (
     <div className="guess-results">
       {rows.map((_, index) => (
-        <Guess word={guesses[index]} key={index} />
+        <Guess word={guesses[index]} key={index} answer={answer} />
       ))}
     </div>
   );
